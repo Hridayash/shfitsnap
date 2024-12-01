7e6e934953193258b979bee76906785506e4fd5c
Hridayash Pariyar <hridayashpariyar@Hridayashs-MacBook-Pro.local>
2024-11-30 22:06:44 -0500

    add new crud from Spring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,6 @@ function App() {
          <Route path ="/home" element  = {<Home/>} />
          <Route path ="/profile" element  = {<Profile/>} />
          <Route path="/edit" element={<Edit />} />          
-
          <Route path="/terms" element={<Terms />} />  
          <Route path="/privacy" element={<PrivacyPolicy />} />    
 
